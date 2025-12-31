ebb9e07ca96fc7f79b3931e4aa6688d50a92aac7
probitsdevelopment <probitsdevelopment@gmail.com>
2025-12-31 08:55:55 +0530

    Remove all files from the main branch

--- src/store/actions/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './authActions';
-export * from './projectActions';
-export * from './userActions';
-export * from './sidebarActions';
--- src/store/actions/sidebarActions.ts
+++
@@ -1,6 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const toggleCollapse = createAction('sidebar/toggleCollapse');
-export const setActiveSection = createAction<string>('sidebar/setActiveSection');
-export const toggleMenu = createAction<string>('sidebar/toggleMenu');
-export const expandMenu = createAction<string>('sidebar/expandMenu');
